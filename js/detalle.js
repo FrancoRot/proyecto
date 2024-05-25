37987f03f6049f2556872a8014e703b33e06ff95
Virginia <128263468+Virtar@users.noreply.github.com>
2024-05-25 09:56:52 -0300

    version 5.2

--- js/detalle.js
+++ js/detalle.js
@@ -7,6 +7,7 @@ const urlParams = new URLSearchParams(queryString);
 const id = urlParams.get('id');
 console.log(id);
 inicializacion(id);
+let nombreValido, apellidoValido, correoValido, telefonoValido, cantidadValido, tipo_eventoValido ,fechaValida= false;
 
 function inicializacion(numero) {
     // enlaces
@@ -15,7 +16,7 @@ function inicializacion(numero) {
     let imagen3 = document.getElementById(`imagen3`);
     let imagen4 = document.getElementById(`imagen4`);
     // carga de imagenes
-    imagen1.src ="."+ datos[parseInt(numero)].foto1;
+    imagen1.src = "." + datos[parseInt(numero)].foto1;
     imagen2.src = datos[parseInt(numero)].foto2;
     imagen3.src = datos[parseInt(numero)].foto3;
     imagen4.src = datos[parseInt(numero)].foto4;
@@ -74,11 +75,14 @@ function mostrarFormulario() {
 // escuchador de boton reservar
 document.getElementById("botonReservar").addEventListener("click", function (event) {
     event.preventDefault(); // Evita el comportamiento predeterminado del botón
-    let formulario = document.getElementById("formulario");
-    // para ocultar el formulario primero hay que sacar el centrado
-    formulario.classList.remove("centrado");
-    // Oculta la sección de formulario al presionar el botón de reserva
-    formulario.classList.add("oculto");
+    if (nombreValido && apellidoValido && correoValido && telefonoValido && cantidadValido && tipo_eventoValido && fechaValida) {
+        let formulario = document.getElementById("formulario");
+        // para ocultar el formulario primero hay que sacar el centrado
+        formulario.classList.remove("centrado");
+        // Oculta la sección de formulario al presionar el botón de reserva
+        formulario.classList.add("oculto");
+    }
+
 });
 
 function botonMapa() {
@@ -94,7 +98,7 @@ function botonMapa() {
 
 function botonCaracteristicas() {
     let caracteristicas = document.getElementById("caracteristicas");
-    
+
     if (caracteristicas.classList.contains("oculto")) {
         caracteristicas.classList.remove("oculto");
         caracteristicas.classList.add("container");
@@ -108,27 +112,31 @@ function botonCaracteristicas() {
 
 function botonReservar2() {
     let formulario = document.getElementById("formulario");
-
+    //si esta oculto lo muestra
     if (formulario.classList.contains("oculto")) {
         formulario.classList.remove("oculto");
         formulario.classList.add("container");
         formulario.classList.add("centrado");
-    } else {
+    } else { // si esta visible entonces lo oculta
+
         formulario.classList.remove("container");
         formulario.classList.remove("centrado");
         formulario.classList.add("oculto");
+
+
     }
-    }
-    const expresiones = {
-        nombre: /^[a-zA-ZáéíóúÁÉÍÓÚüÜ\s]+$/,
-        apellido: /^[a-zA-ZáéíóúÁÉÍÓÚüÜ\s]+$/,
-        correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-        telefono: /^\d{7,14}$/, // 7 a 14 numeros.
-        numeros : /^\d{1,7}$/ // De 1 a 7 números
-    }
-    function comprobarCampo(expresion, valor) {
-        return expresion.test(valor);
-    }
+}
+const expresiones = {
+    nombre: /^[a-zA-ZáéíóúÁÉÍÓÚüÜ\s]+$/,
+    apellido: /^[a-zA-ZáéíóúÁÉÍÓÚüÜ\s]+$/,
+    correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    telefono: /^\d{7,14}$/, // 7 a 14 numeros.
+    numeros: /^\d{1,7}$/ // De 1 a 7 números
+}
+function comprobarCampo(expresion, valor) {
+
+    return expresion.test(valor);
+}
 //retorna false si hay un campo vacio
 function camposValidos() {
     const nombre = document.querySelector("#nombre");
@@ -137,22 +145,111 @@ function camposValidos() {
     const telefono = document.querySelector("#telefono");
     const cantidad = document.querySelector("#cantidad");
     const tipo_evento = document.querySelector("#tipo_evento");
-    if ((nombre.value) === ""|| comprobarCampo(expresiones.nombre, nombre.value)===false) return false;
-    if ((apellido.value) === "" || comprobarCampo(expresiones.apellido, apellido.value)===false) return false;
-    if ((correo.value) === "" || comprobarCampo(expresiones.correo, correo.value)===false) return false;
-    if ((telefono.value) === "" || comprobarCampo(expresiones.telefono, telefono.value)===false) return false;
-    if ((cantidad.value) === "" || comprobarCampo(expresiones.numeros, cantidad.value)===false) return false;
-    if ((tipo_evento.value) === "") return false;
-    return true;
+    let error = false;
+
+    if ((nombre.value) === "" || comprobarCampo(expresiones.nombre, nombre.value) === false) {
+        nombreValido = false;
+        error = true;
+
+    } else {
+
+        nombreValido = true;
+    };
+    if ((apellido.value) === "" || comprobarCampo(expresiones.apellido, apellido.value) === false) {
+        apellidoValido = false;
+        error = true;
+
+    } else {
+        apellidoValido = true;
+    };
+    if ((correo.value) === "" || comprobarCampo(expresiones.correo, correo.value) === false) {
+        correoValido = false;
+        error = true;
+
+    } else {
+        correoValido = true;
+    }
+    if ((telefono.value) === "" || comprobarCampo(expresiones.telefono, telefono.value) === false) {
+        telefonoValido = false;
+        error = true;
+  
+    } else {
+        telefonoValido = true;
+    }
+    if ((cantidad.value) === "" || comprobarCampo(expresiones.numeros, cantidad.value) === false) {
+        cantidadValido = false;
+        error = true;
+  
+    } else {
+        cantidadValido = true;
+    }
+    if ((tipo_evento.value) === "") {
+        tipo_eventoValido = false;
+        error = true;
+    
+    } else {
+        tipo_eventoValido = true;
+    }
+    // if ((nombre.value) === "" || comprobarCampo(expresiones.nombre, nombre.value) === false) return false;
+    // if ((apellido.value) === "" || comprobarCampo(expresiones.apellido, apellido.value) === false) return false;
+    // if ((correo.value) === "" || comprobarCampo(expresiones.correo, correo.value) === false) return false;
+    //if ((telefono.value) === "" || comprobarCampo(expresiones.telefono, telefono.value) === false) return false;
+    // if ((cantidad.value) === "" || comprobarCampo(expresiones.numeros, cantidad.value) === false) return false;
+    // if ((tipo_evento.value) === "") return false;
+    return error;
 }
+// se activa al presionar el boton confirmar
 function obtenerDatos() {
-    let validacion = camposValidos();
-    if (!validacion) {
-        alert("Todos los campos son obligatorios, por favor veifique todos los campos.");
+    let mensaje = document.getElementById("mensajeNombre");
+    mensaje.style.color = "#666"
+    mensaje = document.getElementById("mensajeApellido");
+    mensaje.style.color = "#666"
+    mensaje = document.getElementById("mensajeEmail");
+    mensaje.style.color = "#666"
+    mensaje = document.getElementById("mensajeTelefono");
+    mensaje.style.color = "#666"
+    mensaje = document.getElementById("mensajeCantidad");
+    mensaje.style.color = "#666"
+    mensaje = document.getElementById("mensajeFecha");
+    mensaje.style.color = "#666"
+
+    let hayError = camposValidos();
+    //  console.log("nombre valido: "+nombreValido);
+    if (hayError) {
+        alert("error: uno de los campos no es valido");
+        if (!nombreValido) {
+            //  alert("error: El nombre no es valido,ingresar solo letras");
+            let mensaje = document.getElementById("mensajeNombre");
+            mensaje.style.color = "red"
+        }
+        if (!apellidoValido) {
+            //         alert("error: El apellido no es valido,ingresar solo letras");
+            let mensaje = document.getElementById("mensajeApellido");
+            mensaje.style.color = "red"
+        }
+        if (!correoValido) {
+            // alert("error: La direccion de correo no es valida,ingresar un correo valido");
+            let mensaje = document.getElementById("mensajeEmail");
+            mensaje.style.color = "red"
+        }
+        if (!telefonoValido) {
+            //   alert("error: el numero de telefono no es valido,ingresar solo numeros (7 a 14 digitos)");
+            let mensaje = document.getElementById("mensajeTelefono");
+            mensaje.style.color = "red"
+        }
+        if (!cantidadValido) {
+            //  alert("error: el numero de invitados no es valido,ingresar solo numeros");
+            let mensaje = document.getElementById("mensajeCantidad");
+            mensaje.style.color = "red"
+        }
+        // alert("Todos los campos son obligatorios, por favor veifique todos los campos.");
     } else {// validacion campo fecha
         let fecha = obtenerFecha();
         if (fecha === null) {
+            fechaValida=false;
             alert("fecha invalida");
+            let mensaje = document.getElementById("mensajeFecha");
+            mensaje.style.color = "red"
         } else {
             // obtengo el vector de reservas
             let reservas = datos[parseInt(id)].reservas;
@@ -161,14 +258,17 @@ function obtenerDatos() {
                 reservas.push(fecha);
                 datos[parseInt(id)].reservas = reservas;
                 alert("se envia los datos al servidor");
+                fechaValida=true;
             } else {// si tiene reservas entonces verifico disponibilidad
                 // devuelve false si encuentra la fecha y no esta disponible
                 if (verificarDisponibilidad(reservas, fecha)) {
                     reservas.push(fecha);
                     datos[parseInt(id)].reservas = reservas;
                     alert("se envia los datos al servidor");
+                    fechaValida=true;
                 } else {
                     alert("no esta disponible para esa fecha");
+                    fechaValida=false;
                 }
             }
         }
@@ -176,7 +276,10 @@ function obtenerDatos() {
     }
 
 }
-
+function cambiarColor() {
+    var mensaje = document.getElementById("mensaje");
+    mensaje.classList.add("red-text");
+}
 function obtenerFecha() {
     // Obtener el valor del input de fecha
     let fechaInput = document.getElementById("fecha").value;
